b7a917a049ad095292ea154c6c410e5f6d113719
ibti-m <ibtihalmossa@gmail.com>
2026-01-29 20:04:18 -0500

    Added the map and toggle functionality

--- backend/src/services/map.js
+++ backend/src/services/map.js
@@ -1,1 +1,10 @@
-// this is for business logic starting with the map
+function getCampusCoordinates(campusName) {
+  const campuses = {
+    SGW: { lat: 45.4974, lng: -73.5771 },
+    Loyola: { lat: 45.4587, lng: -73.6409 },
+  };
+
+  return campuses[campusName] || null;
+}
+
+module.exports = { getCampusCoordinates };
--- frontend/where2go/App.js
+++ frontend/where2go/App.js
@@ -1,12 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
+import { GOOGLE_MAPS_API_KEY } from '@env';
 
 export default function App() {
+  const [currentCampus, setCurrentCampus] = useState('SGW');
+  const [campusCoords, setCampusCoords] = useState({ lat: 45.4974, lng: -73.5771 });
+  const mapRef = useRef(null);
+  // whenever currentCampus changes, this will get the new coordinates from the backend
+    useEffect(() => {
+      fetch(`http://localhost:3000/campus/${currentCampus}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setCampusCoords(data);
+  // This will center the map on the new coords
+          mapRef.current?.setCenter(data);
+        })
+        .catch((err) => console.error('Error fetching campus coordinates:', err));
+    }, [currentCampus]);
   return (
+  <APIProvider apiKey={GOOGLE_MAPS_API_KEY} onLoad={() => console.log('Maps API has loaded.')}>
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Map
+        ref={mapRef}
+        defaultZoom={13}
+        defaultCenter={campusCoords} // Location that gets first loaded
+        style={{ width: '100%', height: '100%' }}
+        onCameraChanged={(ev) =>
+          console.log(
+            'camera changed:',
+            ev.detail.center,
+            'zoom:',
+            ev.detail.zoom
+          )
+        }
+      >
+        <Marker position={campusCoords} />
+        </Map>
+         <View style={styles.buttons}>
+                  <Button title="SGW" onPress={() => setCurrentCampus('SGW')} />
+                  <Button title="Loyola" onPress={() => setCurrentCampus('Loyola')} />
+                </View>
       <StatusBar style="auto" />
     </View>
+       </APIProvider>
   );
 }
 
