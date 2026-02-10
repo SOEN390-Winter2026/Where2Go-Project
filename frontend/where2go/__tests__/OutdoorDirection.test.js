c4ac498f2ccd2a0a43f1c22c73ae88d00f49e1f3
Steven01231 <100371532+Steven01231@users.noreply.github.com>
2026-02-10 13:47:44 -0500

    Added Outdoor Direction test file
    - Test render of the class

---
+++ frontend/where2go/__tests__/OutdoorDirection.test.js
@@ -0,0 +1,8 @@
+import OutdoorDirection from '../src/OutdoorDirection.js';
+
+describe("Rendering Features Properly", () => {
+
+    it("Render SideLeftBar", () => {
+        expect(OutdoorDirection).toBeDefined();
+    });
+});
