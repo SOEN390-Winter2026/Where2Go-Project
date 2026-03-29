f1a4cbd6e6abbe7e2610ea60cd8f813445a650b8
Adriana643 <a.d.r.i@hotmail.ca>
2026-03-29 13:45:10 -0400

    Small Jest fix for indoors pois

--- client/src/IndoorSideLeftBar.js
+++ client/src/IndoorSideLeftBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Image, Pressable } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import PropTypes from "prop-types";
@@ -24,8 +24,18 @@ export default function IndoorSideLeftBar({
     const firstPOIIcon = require("../assets/gis--poi-alt.png");
     const secondPOIIcon = require("../assets/gis--poi-alt-2.png");
     const [activeSearch, setActiveSearch] = useState(false);
+    const [internalPOIEnabled, setInternalPOIEnabled] = useState(isPOIEnabled);
 
-    const activePOI = isPOIEnabled ? "poi" : null;
+    useEffect(() => {
+        setInternalPOIEnabled(isPOIEnabled);
+    }, [isPOIEnabled]);
+
+    const handlePOIPress = () => {
+        setInternalPOIEnabled(prev => !prev);
+        onTogglePOI();
+    };
+
+    const activePOI = internalPOIEnabled ? "poi" : null;
     const leftBarActiveInputs = { isAccessibilityEnabled, activePOI };
     const iconPairs = {
         disability: [firstDisabilityIcon, secondDisabilityIcon],
@@ -48,7 +58,7 @@ export default function IndoorSideLeftBar({
             </Pressable>
 
             {/* Directions or search (?) to be changed if needed */}
-            <Pressable testID="search-btn" //btw this should lead to another page once implemented for indoors directions
+            <Pressable testID="search-btn"
                 style={[sideLeftBarSharedStyles.barItem, { backgroundColor: activeSearch ? LEFT_BAR_BURGUNDY : LEFT_BAR_GREY }]}
                 onPress={handleSearchPress}
             >
@@ -70,7 +80,7 @@ export default function IndoorSideLeftBar({
             {/* POIs */}
             <Pressable testID="poi-btn"
                 style={[sideLeftBarSharedStyles.barItem, leftBarIconState("poi", leftBarActiveInputs)]}
-                onPress={onTogglePOI}
+                onPress={handlePOIPress}
             >
                 <Image source={leftBarIconSource("poi", leftBarActiveInputs, iconPairs)} style={sideLeftBarSharedStyles.icon} />
             </Pressable>
