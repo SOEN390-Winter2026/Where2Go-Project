039135c1acab621845f3194ce1cdd55f3eb263cb
ibti-m <ibtihalmossa@gmail.com>
2026-03-29 16:09:55 -0400

    fixes

--- client/src/services/indoorAccessibleRouting.js
+++ client/src/services/indoorAccessibleRouting.js
@@ -5,9 +5,6 @@ import { extractFloorPlan } from "../utils/floorPlanUtils";
 // Cache graph build per building+rules
 const GRAPH_CACHE = new Map();
 
-// Cache graph build per building+rules
-const GRAPH_CACHE = new Map();
-
 /**
  * UTILITIES
  */
--- client/src/utils/useIndoorMaps.js
+++ client/src/utils/useIndoorMaps.js
@@ -2,7 +2,6 @@ import { useState, useRef, useEffect } from 'react';
 import { Animated, PanResponder } from 'react-native';
 import { indoorMaps } from '../data/indoorData';
 import { extractFloorPlan } from './floorPlanUtils';
-import { API_BASE_URL } from '../config';
 
 
 // Indoor JSON for this campus only (avoids mixing Loyola/SGW when the directions list includes all API codes)
